062286caf53e5060dbce20bec3135b312f532eb3
MrW0nder <jrjgratton@gmail.com>
2026-02-10 12:32:06 -0700

    Update vite.config.mjs

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   root: '.', // Root at project folder
   publicDir: 'public', // Static assets
